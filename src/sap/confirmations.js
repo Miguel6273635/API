2539454a3a3a64739eff99c2c1a06d5889b10339
Miguel6273635 <132727142+Miguel6273635@users.noreply.github.com>
2026-07-21 13:15:40 -0600

    fix: terminate confirmation sessions

--- src/sap/confirmations.js
+++ src/sap/confirmations.js
@@ -1,28 +1,30 @@
 // src/sap/confirmations.js
-import { executeHttpRequest } from "@sap-cloud-sdk/http-client";
 import { SAP_CLIENT, SAP_LANG } from "../config/env.js";
 import { getSapDestination } from "./destination.js";
-import { fetchCsrfAndCookies } from "./csrf.js";
+import { fetchCsrfAndCookies, forwardWrite } from "./csrf.js";
 
 export async function postConfirmation(payload) {
-  const destination = await getSapDestination(); // <- destination REAL con .url
+  const destination = await getSapDestination();
   const serviceName = "ZCS_CREATE_CONFIRMATION_SRV";
   const servicePath = `/sap/opu/odata/sap/${serviceName}`;
 
-  const { csrfToken, cookies } = await fetchCsrfAndCookies(destination, serviceName);
+  const { csrfToken, cookies } = await fetchCsrfAndCookies(
+    destination,
+    serviceName
+  );
 
-  const resp = await executeHttpRequest(destination, {
+  const resp = await forwardWrite({
+    destination,
     method: "POST",
-    url: `${servicePath}/ConfirmationHeaderSet`,
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRF-Token": csrfToken,
-      Cookie: cookies,
-      Accept: "application/json",
-      "sap-client": SAP_CLIENT,
-      "Accept-Language": SAP_LANG || "ES",
-    },
-    data: payload,
+    path:
+      `${servicePath}/ConfirmationHeaderSet` +
+      `?sap-client=${encodeURIComponent(SAP_CLIENT)}` +
+      `&sap-language=${encodeURIComponent(SAP_LANG || "ES")}`,
+    body: payload,
+    csrfToken,
+    cookies,
+    contentType: "application/json",
+    terminateSession: true,
   });
 
   return resp.data;
